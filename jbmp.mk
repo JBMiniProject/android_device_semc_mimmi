caaa8c7fe6e326a6c980a2b0c20cb1617eef107e
Daveee10 <daver16@msn.com>
2012-09-29 16:41:43 +0200

    Add TARGET_INCLUDE_GAPPS and include jbmp vendor

--- jbmp.mk
+++ jbmp.mk
@@ -1,16 +1,14 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := U20i
-
 # Inherit some common CM stuff.
-# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/semc/mimmi/device_mimmi.mk)
 
 # Setup device configuration
+PRODUCT_RELEASE_NAME := U20i
 PRODUCT_DEVICE := mimmi
 PRODUCT_NAME := jbmp_mimmi
 PRODUCT_BRAND := SEMC
@@ -21,6 +19,7 @@ PRODUCT_MANUFACTURER := Sony Ericsson
 PRODUCT_VERSION_DEVICE_SPECIFIC := 3
 TARGET_NO_LIVEWALLPAPERS := false
 TARGET_BOOTANIMATION_NAME := vertical-240x320
+TARGET_INCLUDE_GAPPS := false
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=U20 BUILD_FINGERPRINT="google/yakju/maguro:4.1.1/JRO03C/398337:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03C 398337 release-keys"
